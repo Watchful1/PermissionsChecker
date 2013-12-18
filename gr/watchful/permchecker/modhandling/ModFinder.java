bcbd672a7d1f25dedcd96474f3fd7f9f1ec6e5f0
Watchful1 <Watchful@vt.edu>
2013-12-17 22:06:35 -0500

    Start on package restructuring

---
+++ gr/watchful/permchecker/modhandling/ModFinder.java
@@ -0,0 +1,13 @@
+package gr.watchful.permchecker.modhandling;
+
+public class ModFinder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
