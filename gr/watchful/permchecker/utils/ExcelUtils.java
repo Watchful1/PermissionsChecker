7f0f2bd358aa9b7f5a7ec49bcf8dd7afb04dddd7
Andrew Wong <andrew.j.wong@outlook.com>
2014-01-16 20:54:23 +1100

    Last Updated, OS Check
    Started last updated checks
    OS System check
    Made spreadsheet a permanent file.

--- gr/watchful/permchecker/utils/ExcelUtils.java
+++ gr/watchful/permchecker/utils/ExcelUtils.java
@@ -1,7 +1,7 @@
 /**
  * 
  * Source: http://viralpatel.net/blogs/java-read-write-excel-file-apache-poi/
- * ~bearbear12345~ 
+ * @author bearbear12345
  * 
  */
 package gr.watchful.permchecker.utils;
@@ -18,16 +18,21 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
-	public static ArrayList<ArrayList<String>> ttoArray(File file, int sheetNum) throws FileNotFoundException, IOException {
-		ArrayList<ArrayList<String>> rows = new ArrayList<ArrayList<String>>();//TODO actually return the thing
-		
+	public static ArrayList<ArrayList<String>> toArray(File file, int sheetNum)
+			throws FileNotFoundException, IOException {
+		ArrayList<ArrayList<String>> rows = new ArrayList<ArrayList<String>>();// TODO
+																				// actually
+																				// return
+																				// the
+																				// thing
+
 		FileInputStream input = new FileInputStream(file);
 		XSSFWorkbook workbook = new XSSFWorkbook(input);
 		XSSFSheet sheet = workbook.getSheetAt(sheetNum);
-		for(Row row : sheet) {
-			for(Cell cell : row) {
-				if(cell.getCellType() == Cell.CELL_TYPE_STRING) {
-					System.out.print(cell.getStringCellValue()+"  :  ");
+		for (Row row : sheet) {
+			for (Cell cell : row) {
+				if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+					System.out.print(cell.getStringCellValue() + "  :  ");
 				}
 			}
 			System.out.print("\n");
--- mainClass.java
+++ mainClass.java
@@ -8,7 +8,7 @@ import gr.watchful.permchecker.panels.ModFileEditor;
 import gr.watchful.permchecker.panels.NamedScrollingListPanel;
 import gr.watchful.permchecker.utils.ExcelUtils;
 import gr.watchful.permchecker.utils.FileUtils;
-
+import gr.watchful.permchecker.utils.bearbear12345.OsTypes;
 
 import java.awt.CardLayout;
 import java.awt.Dimension;
@@ -31,185 +31,222 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class mainClass extends JFrame implements NamedScrollingListPanelListener {
-	private DefaultListModel<Mod> goodMods;
-	private DefaultListModel<Mod> badMods;
-	private DefaultListModel<ModFile> unknownMods;
-	private NamedScrollingListPanel<Mod> good;
-	private NamedScrollingListPanel<Mod> bad;
-	private NamedScrollingListPanel<ModFile> unknown;
-	private File permFile;
-	private JPanel cards;
-	private ModEditor modEditor;
-	private ModFileEditor modFileEditor;
+    private DefaultListModel<Mod> goodMods;
+    private DefaultListModel<Mod> badMods;
+    private DefaultListModel<ModFile> unknownMods;
+    private NamedScrollingListPanel<Mod> good;
+    private NamedScrollingListPanel<Mod> bad;
+    private NamedScrollingListPanel<ModFile> unknown;
+    private File permFile;
+    private File appstore; //Location for the spreadsheet file
+    private JPanel cards;
+    private ModEditor modEditor;
+    private ModFileEditor modFileEditor;
 
-	public mainClass() {
-		goodMods = new DefaultListModel<Mod>();
-		badMods = new DefaultListModel<Mod>();
-		unknownMods = new DefaultListModel<ModFile>();
+    public mainClass() {
+        goodMods = new DefaultListModel<Mod>();
+        badMods = new DefaultListModel<Mod>();
+        unknownMods = new DefaultListModel<ModFile>();
 
-		try {
-			permFile = File.createTempFile("PermissionsCheckerPermFile",
-					".xlsx");
-		} catch (IOException e) {
-			// TODO Tell user error (No perms?)
-		}
+        /**
+         * Check which OS the system is running, and make the appropriate directories if necessary
+         * @author bearbear12345
+         */
+        OsTypes.OSType ostype = OsTypes.getOperatingSystemType();
+        System.out.println("Operating System: " + ostype.toString());
+        System.out.println("Searching for application storage directory...");
+        switch (ostype) {
+            case Windows:
+                appstore = new File(System.getenv("APPDATA") + "/ftb/PermissionsChecker");
+                break;
+            case MacOS:
+                appstore = new File(System.getProperty("user.home") + "/Library/Application Support/ftb/PermissionsChecker");
+                break;
+            case Linux:
+                appstore = new File(System.getProperty("user.home") + "/.ftb/PermissionsChecker");
+                break;
+            case Other:
+                //TODO ????
+                break;
+        }
+        if (!appstore.exists()) {
+            System.out.println("Directory not found! Creating directory: " + appstore.getPath());
+            boolean result = appstore.mkdirs();
+            if (result) {
+                System.out.println(appstore.getPath() + " created!");
+            }
+        } else {
+            System.out.println("Directory exists!");
+        }
 
-		this.setTitle("Permissions Checker"); // Set the window title
-		this.setPreferredSize(new Dimension(600, 300)); // and the initial size
 
-		//TODO move this stuff to a seperate method
-		File currentDir = new File(System.getProperty("user.dir"));
-		if(isMinecraftDir(currentDir)) {
-			discoverMods(currentDir);
-		} else if(isMinecraftDir(currentDir.getParentFile())) {
-			discoverMods(currentDir.getParentFile());
-		} else {
-			//TODO implement a selection of modpacks, maybe save modpack locations for later use
-			//also allow selecting of multiMC instances folder
-			
-			//debug
-			discoverMods(new File("C:\\Users\\Gregory\\Desktop\\MultiMC\\instances\\Hammercraft 4.3.0 Custom\\minecraft"));
-		}
+        // Checks if PermissionsCheckerPermFile.xlsx is existent
+        /*
+         * Old Code permFile = File.createTempFile("PermissionsCheckerPermFile",
+         * ".xlsx");
+         */
+        permFile = new File(appstore.getPath() + "/PermissionsChecker.data");
+        if (!permFile.exists()) {
+            try {
+                permFile.createNewFile();
+            } catch (IOException e) {
+                System.out.println("An error occured while creating during setup. Please try again later. If issues persist, contact the author");
+            }
+        }
+        //TODO I guess first run should download? - bearbear12345
 
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-		mainPanel.setAlignmentX(0f);
-		this.add(mainPanel);
+        this.setTitle("Permissions Checker"); // Set the window title
+        this.setPreferredSize(new Dimension(600, 300)); // and the initial size
 
-		good = new NamedScrollingListPanel<Mod>(
-				"Good", new Dimension(100, 300), goodMods);
-		good.addListener(this);
-		mainPanel.add(good);
-		bad = new NamedScrollingListPanel<Mod>(
-				"Bad", new Dimension(100, 300), badMods);
-		bad.addListener(this);
-		mainPanel.add(bad);
-		unknown = new NamedScrollingListPanel<ModFile>(
-				"Unknown", new Dimension(100, 300), unknownMods);
-		mainPanel.add(unknown);
-		unknown.addListener(this);
+        // TODO move this stuff to a seperate method
+        File currentDir = new File(System.getProperty("user.dir"));
+        if (isMinecraftDir(currentDir)) {
+            discoverMods(currentDir);
+        } else if (isMinecraftDir(currentDir.getParentFile())) {
+            discoverMods(currentDir.getParentFile());
+        } else {
+            // TODO implement a selection of modpacks, maybe save modpack
+            // locations for later use
+            // also allow selecting of multiMC instances folder
 
-		JPanel newWindow = new JPanel();
-		JPanel modEditWindow = new JPanel();
+            // debug
 
-		cards = new JPanel(new CardLayout());
-		cards.setMinimumSize(new Dimension(300, 300));
-		cards.add(newWindow);
-		cards.add(modEditWindow);
-		
-		modEditor = new ModEditor(new Dimension(300,300));
-		cards.add(modEditor,"MODEDITOR");
-		modFileEditor = new ModFileEditor(new Dimension(300,300));
-		cards.add(modFileEditor,"MODFILEEDITOR");
-		
-		CardLayout cardLayout = (CardLayout)(cards.getLayout());
-		cardLayout.show(cards, "MODEDITOR");
+            // discoverMods(new
+            // File("C:\\Users\\Gregory\\Desktop\\MultiMC\\instances\\Hammercraft 4.3.0 Custom\\minecraft"));
+        }
 
-		mainPanel.add(cards);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+        mainPanel.setAlignmentX(0f);
+        this.add(mainPanel);
 
-		JMenuBar menuBar = new JMenuBar(); // create the menu
-		JMenu menu = new JMenu("Temp"); // with the submenus
-		menuBar.add(menu);
+        good = new NamedScrollingListPanel<Mod>("Good", new Dimension(100, 300), goodMods);
+        good.addListener(this);
+        mainPanel.add(good);
+        bad = new NamedScrollingListPanel<Mod>("Bad", new Dimension(100, 300), badMods);
+        bad.addListener(this);
+        mainPanel.add(bad);
+        unknown = new NamedScrollingListPanel<ModFile>("Unknown", new Dimension(100, 300), unknownMods);
+        mainPanel.add(unknown);
+        unknown.addListener(this);
 
-		JMenuItem updatePerms = new JMenuItem(
-				"Force-update Permissions Listing");
+        JPanel newWindow = new JPanel();
+        JPanel modEditWindow = new JPanel();
 
-		// listen to all the menu items and then add them to the menus
-		updatePerms.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Check last modified - bandwidth
-				try {
-					FileUtils
-							.downloadToFile(
-									new URL(
-											"https://skydrive.live.com/download?resid=96628E67B4C51B81!105&authkey=!AK7mlmHB0nrxmHg&ithint=file%2c.xlsx"),
-									permFile);
-					try {
-						ExcelUtils.ttoArray(permFile,1);
-					} catch (FileNotFoundException e) {
-						System.out.println("UHOH");
-					} catch (IOException e) {
-						System.out.println("uhoh");
-					}
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		menu.add(updatePerms);
-		// TODO Folder select (Pre: Check if current folder is valid)
-		JMenuItem chooseModpack = new JMenuItem("Choose Modpack");
+        cards = new JPanel(new CardLayout());
+        cards.setMinimumSize(new Dimension(300, 300));
+        cards.add(newWindow);
+        cards.add(modEditWindow);
 
-		// listen to all the menu items and then add them to the menus
-		chooseModpack.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				JFileChooser fileChooser = new JFileChooser(System
-						.getProperty("user.home"));
-				fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-				int returnVal = fileChooser.showOpenDialog(null);
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					File result = fileChooser.getSelectedFile();
-					discoverMods(result);
-				}
-			}
-		});
-		menu.add(chooseModpack);
+        modEditor = new ModEditor(new Dimension(300, 300));
+        cards.add(modEditor, "MODEDITOR");
+        modFileEditor = new ModFileEditor(new Dimension(300, 300));
+        cards.add(modFileEditor, "MODFILEEDITOR");
 
-		this.setJMenuBar(menuBar);
+        CardLayout cardLayout = (CardLayout) (cards.getLayout());
+        cardLayout.show(cards, "MODEDITOR");
 
-		pack();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
-	
-	private static boolean isMinecraftDir(File file) {
-		return file.getName().equals("minecraft") || file.getName().equals(".minecraft");
-	}
+        mainPanel.add(cards);
 
-	public static void main(String[] args) {
-		new mainClass();
-	}
+        JMenuBar menuBar = new JMenuBar(); // create the menu
+        JMenu menu = new JMenu("Temp"); // with the submenus
+        menuBar.add(menu);
 
-	private void discoverMods(File minecraftFolder) {
-		ModFinder.discoverAllMods(minecraftFolder, unknownMods, badMods);
-	}
+        JMenuItem updatePerms = new JMenuItem("Update Permissions Info");
 
-	public void selectionChanged(NamedSelectionEvent event) {
-		System.out.println(event.getParentName()+" : "+event.getSelected());
-		updateEditor(event.getParentName(),event.getSelected());
-	}
-	
-	private void updateEditor(String list, int selected) {
-		if(list.equals("Good")) {
-			bad.clearSelection();
-			unknown.clearSelection();
-			
-			CardLayout cardLayout = (CardLayout)(cards.getLayout());
-			cardLayout.show(cards, "MODEDITOR");
-			
-			modEditor.setMod(good.getSelected());
-		}
-		if(list.equals("Bad")) {
-			good.clearSelection();
-			unknown.clearSelection();
-			
-			CardLayout cardLayout = (CardLayout)(cards.getLayout());
-			cardLayout.show(cards, "MODEDITOR");
+        // listen to all the menu items and then add them to the menus
+        updatePerms.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
 
-			modEditor.setMod(bad.getSelected());
-		}
-		if(list.equals("Unknown")) {
-			good.clearSelection();
-			bad.clearSelection();
-			
-			CardLayout cardLayout = (CardLayout)(cards.getLayout());
-			cardLayout.show(cards, "MODFILEEDITOR");
+                // TODO Check last modified - bandwidth
+                try {
 
-			modFileEditor.setModFile(unknown.getSelected());
-		}
-	}
+                    FileUtils.downloadToFile(new URL("https://skydrive.live.com/download?resid=96628E67B4C51B81!105&authkey=!AK7mlmHB0nrxmHg"), permFile);
+                    try {
+                        ExcelUtils.toArray(permFile, 1);
+                    } catch (FileNotFoundException e) {
+                        System.out.println("The file has been moved or deleted. Please contact the author immediately about this issue.");
+                    } catch (IOException e) {
+                        System.out.println("An error occured while saving the file. Please try again later. If issues persist, contact the author");
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        menu.add(updatePerms);
+        // TODO Folder select (Pre: Check if current folder is valid)
+        JMenuItem chooseModpack = new JMenuItem("Choose Modpack");
+
+        // listen to all the menu items and then add them to the menus
+        chooseModpack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                JFileChooser fileChooser = new JFileChooser(System.getProperty("user.home"));
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int returnVal = fileChooser.showOpenDialog(null);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File result = fileChooser.getSelectedFile();
+                    discoverMods(result);
+                }
+            }
+        });
+        menu.add(chooseModpack);
+
+        this.setJMenuBar(menuBar);
+
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    private static boolean isMinecraftDir(File file) {
+        return file.getName().equals("minecraft") || file.getName().equals(".minecraft");
+    }
+
+    public static void main(String[] args) {
+        new mainClass();
+    }
+
+    private void discoverMods(File minecraftFolder) {
+        ModFinder.discoverAllMods(minecraftFolder, unknownMods, badMods);
+    }
+
+    public void selectionChanged(NamedSelectionEvent event) {
+        System.out.println(event.getParentName() + " : " + event.getSelected());
+        updateEditor(event.getParentName(), event.getSelected());
+    }
+
+    private void updateEditor(String list, int selected) {
+        if (list.equals("Good")) {
+            bad.clearSelection();
+            unknown.clearSelection();
+
+            CardLayout cardLayout = (CardLayout) (cards.getLayout());
+            cardLayout.show(cards, "MODEDITOR");
+
+            modEditor.setMod(good.getSelected());
+        }
+        if (list.equals("Bad")) {
+            good.clearSelection();
+            unknown.clearSelection();
+
+            CardLayout cardLayout = (CardLayout) (cards.getLayout());
+            cardLayout.show(cards, "MODEDITOR");
+
+            modEditor.setMod(bad.getSelected());
+        }
+        if (list.equals("Unknown")) {
+            good.clearSelection();
+            bad.clearSelection();
+
+            CardLayout cardLayout = (CardLayout) (cards.getLayout());
+            cardLayout.show(cards, "MODFILEEDITOR");
+
+            modFileEditor.setModFile(unknown.getSelected());
+        }
+    }
 }
