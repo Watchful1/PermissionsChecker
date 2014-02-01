9eb719019b5aa8e4995df3547a537646764a8270
Andrew Wong <andrew.j.wong@outlook.com>
2014-02-01 19:53:40 +1100

    Changed input method Removed getAccessToken and replaced with a getKey method (Utilities shouldn't have absolute links if the same method is needed for 2 different links)

--- gr/watchful/permchecker/utils/SkydriveUtils.java
+++ gr/watchful/permchecker/utils/SkydriveUtils.java
@@ -9,19 +9,27 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class SkydriveUtils {
-    public static JSONArray getJSON(String url) throws Throwable {
-        BufferedReader input = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
-        System.out.println(input.readLine());
-        JSONArray output = new JSONArray(input.readLine());
-        return output;
+    public static JSONObject getJSON(String url) throws Throwable {
+        BufferedReader streamReader = new BufferedReader(new InputStreamReader(new URL(url).openConnection().getInputStream(), "UTF-8"));
+        StringBuilder responseStrBuilder = new StringBuilder();
+        String line;
+        while ((line = streamReader.readLine()) != null) {
+            responseStrBuilder.append(line);
+        }
+        JSONObject result = new JSONObject(responseStrBuilder.toString());
+        return result;
     }
-    
-    public static String getAccessToken() throws Throwable {
-        JSONArray array = getJSON("https://login.live.com/oauth20_token.srf?client_id=000000004410FE50&redirect_uri=https://login.live.com/oauth20_desktop.srf&grant_type=refresh_token&refresh_token=Chf9!6iNyOsxUtX2uCMG*SKiPuyCsVNuof8bK7avToNEtCbfzYspPLEbuRXdxwjOd8CFO7BpgmyJmVDUnCqZrT6eJgtZ7mCZgkpBUiLRFm8fLHzD2tbYyn!fhJ0I7Da7i!CG05xN8ZfAc*0cOo02bsqkfq!nak!fKtRfOUal1nHjMYdkWPnTQ8a86UxYNm0nJvEvAahJoayNzJ5tvSdsD0Ar8uauOmMyixRiXkoGUvxViQlBfJYeKeifBR1uZkb5f!*JLMA5!zUxNxES9ahzYR!MATG!tnqWtZLzWCYcESEo73YtjVcNAUnf26Ad0SWunHY1C*awrgf7OgwVbiruORR9pyZ*3QcXpK5lpMDCIsAK");
-        String token = array.getString(array.length() - 1);
-        return token;
+
+    public static String getKey(String key, String url) {
+        try {
+            JSONObject jsonresult = getJSON(url);
+            String result = jsonresult.getString(key);
+            return result;
+        } catch (Throwable e) {
+            return e.toString();
+        }
     }
 }
