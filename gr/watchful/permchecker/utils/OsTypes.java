03d938b35a7d2db3aa618b4f2b076ba567a5d00c
Andrew Wong <andrew.j.wong@outlook.com>
2014-02-01 19:54:47 +1100

    Auto Formatter

--- gr/watchful/permchecker/utils/OsTypes.java
+++ gr/watchful/permchecker/utils/OsTypes.java
@@ -8,34 +8,34 @@ package gr.watchful.permchecker.utils;
  * ~bearbear12345~
  */
 public final class OsTypes {
-  /**
-   * types of Operating Systems
-   */
-  public enum OSType {
-    Windows, MacOS, Linux, Other
-  };
+    /**
+     * types of Operating Systems
+     */
+    public enum OSType {
+        Windows, MacOS, Linux, Other
+    };
 
-  protected static OSType detectedOS;
+    protected static OSType detectedOS;
 
-  /**
-   * detected the operating system from the os.name System property and cache
-   * the result
-   * 
-   * @returns - the operating system detected
-   */
-  public static OSType getOperatingSystemType() {
-    if (detectedOS == null) {
-      String OS = System.getProperty("os.name", "generic").toLowerCase();
-      if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
-        detectedOS = OSType.MacOS;
-      } else if (OS.indexOf("win") >= 0) {
-        detectedOS = OSType.Windows;
-      } else if (OS.indexOf("nux") >= 0) {
-        detectedOS = OSType.Linux;
-      } else {
-        detectedOS = OSType.Other;
-      }
-    }
+    /**
+     * detected the operating system from the os.name System property and cache
+     * the result
+     * 
+     * @returns - the operating system detected
+     */
+    public static OSType getOperatingSystemType() {
+        if (detectedOS == null) {
+            String OS = System.getProperty("os.name", "generic").toLowerCase();
+            if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
+                detectedOS = OSType.MacOS;
+            } else if (OS.indexOf("win") >= 0) {
+                detectedOS = OSType.Windows;
+            } else if (OS.indexOf("nux") >= 0) {
+                detectedOS = OSType.Linux;
+            } else {
+                detectedOS = OSType.Other;
+            }
+        }
         return detectedOS;
     }
 }
