7fe550cb8536aa554cedcde2b0763b5950d62e34
Watchful1 <watchful@vt.edu>
2014-07-13 00:49:26 -0400

    And a bit more sorting. Should be able to sort by arbitrary modpack fields(like lastedited) soon. Also the first step towards searching.

--- source/gr/watchful/permchecker/datastructures/SimpleObjectComparator.java
+++ source/gr/watchful/permchecker/datastructures/SimpleObjectComparator.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 public class SimpleObjectComparator implements Comparator<Object> {
     @Override
     public int compare(Object o1, Object o2) {
-        return o1.toString().compareTo(o2.toString());
+		if(o1.getClass().equals(ModPack.class) && o2.getClass().equals(ModPack.class)) {
+			ModPack pack1 = (ModPack) o1;
+			ModPack pack2 = (ModPack) o2;
+			if(pack1.name.equals(pack2.name)) return pack1.shortName.compareTo(pack2.shortName);
+			else return pack1.name.compareTo(pack2.name);
+		} else return o1.toString().compareTo(o2.toString());
     }
 }
