eb6d4be4c9a2c966e6dd1401ae89b6cb7e0b2f91
Watchful1 <watchful@vt.edu>
2014-08-16 23:54:19 -0400

    Add a dropdown to the update tab to select the version to export. Closes #49

--- source/gr/watchful/permchecker/panels/UpdatePanel.java
+++ source/gr/watchful/permchecker/panels/UpdatePanel.java
@@ -7,6 +7,7 @@ import gr.watchful.permchecker.datastructures.UsesPack;
 import gr.watchful.permchecker.utils.FileUtils;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -14,11 +15,13 @@ import java.io.File;
 public class UpdatePanel extends JPanel implements ActionListener, UsesPack {
 	private LabelField packName;
 	private FileSelector selector;
+	JComboBox<String> versionSelector;
 	public PermissionsPanel permPanel;//TODO really should be a better way to do this
 
 
     public UpdatePanel() {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 
         packName = new LabelField("Pack Name");
         packName.lock("Currently opened pack");
@@ -28,8 +31,10 @@ public class UpdatePanel extends JPanel implements ActionListener, UsesPack {
 		selector.addListener(this);
         this.add(selector);
 
-        LabelField version = new LabelField("Version");
-        this.add(version);
+        versionSelector = new JComboBox<>();
+		versionSelector.setMaximumSize(new Dimension(Integer.MAX_VALUE, 20));
+		versionSelector.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+		this.add(versionSelector);
 
 		JButton exportButton = new JButton("Export");
 		exportButton.addActionListener(new ActionListener() {
@@ -43,6 +48,11 @@ public class UpdatePanel extends JPanel implements ActionListener, UsesPack {
 
 	public void setPack(ModPack pack) {
 		packName.setText(pack.name);
+		versionSelector.removeAllItems();
+		for(String version : pack.versions) {
+			versionSelector.addItem(version);
+		}
+		versionSelector.setSelectedItem(pack.recommendedVersion);
 	}
 
 	public void extractPack(File file) {
@@ -110,7 +120,7 @@ public class UpdatePanel extends JPanel implements ActionListener, UsesPack {
 		}
 		File packExportFolder = new File(Globals.getInstance().preferences.exportFolder + File.separator +
 				"privatepacks" + File.separator + Globals.getModPack().shortName + File.separator +
-				Globals.getModPack().recommendedVersion.replaceAll("\\.", "_"));
+				versionSelector.getSelectedItem());
 		FileUtils.zipFolderTo(Globals.getInstance().preferences.workingFolder,
 				new File(packExportFolder + File.separator + Globals.getModPack().getZipName()));
 
