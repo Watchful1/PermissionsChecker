a8fa58a5f214a9b0e7fa78624220fe947827efe4
Watchful1 <Watchful@vt.edu>
2014-09-16 14:55:11 -0400

    More forgeeditor changes, I'm satisfied with it now

--- source/gr/watchful/permchecker/panels/ForgeEditor.java
+++ source/gr/watchful/permchecker/panels/ForgeEditor.java
@@ -11,14 +11,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
-public class ForgeEditor extends JPanel {
+public class ForgeEditor extends JPanel implements ActionListener {
 	private JLabel label;
-	private JComboBox<String> forgeTypeEditor;
 	private JTextField forgeVersionEditor;
     private ChangeListener changeListener;
-    private String oldVersion;
+    private int oldVersion;
     private ForgeType oldType;
-	private String[] values = {"Recommended", "Latest"};
+	private JRadioButton recommendedButton;
+	private JRadioButton latestButton;
+	private ButtonGroup buttonGroup;
 
 	public ForgeEditor(String name) {
         this(name, null);
@@ -26,7 +27,7 @@ public class ForgeEditor extends JPanel {
 
     public ForgeEditor(String name, ChangeListener changeListener) {
         this.changeListener = changeListener;
-        oldVersion = "";
+        oldVersion = -1;
         oldType = ForgeType.RECOMMENDED;
 
 		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
@@ -38,21 +39,22 @@ public class ForgeEditor extends JPanel {
 		label.setPreferredSize(new Dimension(90, 21));
 		this.add(label);
 
-		forgeTypeEditor = new JComboBox<>(values);
-		forgeTypeEditor.setMaximumSize(new Dimension(120, 21));
-        forgeTypeEditor.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(oldType.equals(getForgeType())) return;
-                oldType = getForgeType();
-				setForgeVersion(-1);
-                notifyChanged();
-            }
-        });
-		this.add(forgeTypeEditor);
-
 		this.add(Box.createHorizontalGlue());
 
+		buttonGroup = new ButtonGroup();
+		this.add(new JLabel("Recommended"));
+		recommendedButton = new JRadioButton();
+		recommendedButton.addActionListener(this);
+		buttonGroup.add(recommendedButton);
+		this.add(recommendedButton);
+
+		this.add(new JLabel("  Latest"));
+		latestButton = new JRadioButton();
+		latestButton.addActionListener(this);
+		buttonGroup.add(latestButton);
+		this.add(latestButton);
+
+		this.add(new JLabel("  Version"));
 		forgeVersionEditor = new JTextField();
 		forgeVersionEditor.setMaximumSize(new Dimension(120, 21));
 		forgeVersionEditor.setPreferredSize(new Dimension(120, 21));
@@ -64,8 +66,10 @@ public class ForgeEditor extends JPanel {
 
             @Override
             public void focusLost(FocusEvent e) {
-                if(oldVersion.equals(forgeVersionEditor.getText())) return;
-                oldVersion = forgeVersionEditor.getText();
+                if(oldVersion == getForgeVersion()) return;
+				if(getForgeVersion() == -1) setForgeType(ForgeType.RECOMMENDED);
+				else setForgeType(ForgeType.VERSION);
+                oldVersion = getForgeVersion();
                 notifyChanged();
             }
         });
@@ -74,31 +78,63 @@ public class ForgeEditor extends JPanel {
 
 	public void setForgeType(ForgeType forgeType) {
 		oldType = forgeType;
-		forgeTypeEditor.setSelectedItem(forgeType);
+		if(forgeType.equals(ForgeType.RECOMMENDED)) {
+			setForgeVersion(-1);
+			latestButton.setSelected(false);
+
+			recommendedButton.setSelected(true);
+		} else if(forgeType.equals(ForgeType.LATEST)) {
+			setForgeVersion(-1);
+			recommendedButton.setSelected(false);
+
+			latestButton.setSelected(true);
+		} else {
+			buttonGroup.clearSelection();
+		}
 	}
 
 	public void setForgeVersion(int forgeVersion) {
-		oldVersion = Integer.toString(forgeVersion);
-		forgeVersionEditor.setText(Integer.toString(forgeVersion));
+		oldVersion = forgeVersion;
+		if(forgeVersion == -1) {
+			forgeVersionEditor.setText("");
+		} else {
+			forgeVersionEditor.setText(Integer.toString(forgeVersion));
+		}
 	}
 
 	public ForgeType getForgeType() {
-		if(getForgeVersion() != -1) {
-			return ForgeType.VERSION;
-		} else if(forgeTypeEditor.getSelectedItem().equals(values[0])) {
+		if(recommendedButton.isSelected()) {
 			return ForgeType.RECOMMENDED;
-		} else if(forgeTypeEditor.getSelectedItem().equals(values[1])) {
+		} else if(latestButton.isSelected()) {
 			return ForgeType.LATEST;
+		} else if(getForgeVersion() != -1) {
+			return ForgeType.VERSION;
 		}
 		return null; //shouldn't happen
 	}
 
 	public int getForgeVersion() {
-		return Integer.parseInt(forgeVersionEditor.getText());
+		if(forgeVersionEditor.getText().equals("")) return -1;
+
+		int version;
+		try {
+			version = Integer.parseInt(forgeVersionEditor.getText());
+		} catch (Exception e) {
+			return -1;
+		}
+		return version;
 	}
 
     public void notifyChanged() {
         if(changeListener == null) return;
         changeListener.stateChanged(new ChangeEvent(this));
     }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(oldType.equals(getForgeType())) return;
+		oldType = getForgeType();
+		setForgeVersion(-1);
+		notifyChanged();
+	}
 }
