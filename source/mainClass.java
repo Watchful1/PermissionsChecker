e9b562e3bc2d320e6bc8ed9ffcb10880444abb26
Watchful1 <Watchful@vt.edu>
2014-11-03 17:46:52 -0500

    Bump version to 1.1.4

--- source/mainClass.java
+++ source/mainClass.java
@@ -37,7 +37,7 @@ public class mainClass extends JFrame implements ListsPacks {
         Globals.getInstance().loadPreferences();
         Globals.getInstance().updateListings();
 
-		this.setTitle("Permissions Checker v 1.1.3"); // Set the window title
+		this.setTitle("Permissions Checker v 1.1.4"); // Set the window title
 		this.setPreferredSize(new Dimension(1000, 600)); // and the initial size
 
         JPanel leftPanel = new JPanel();
